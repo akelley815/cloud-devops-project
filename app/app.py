91196ed3fa813b7ba9d09e6f01b8a75a1ce6034a
akelley815 <aaron.kelley815@outlook.com>
2026-04-20 11:23:46 -0500

    Resolved merge conflict

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Cloud DevOps Project Running!"
+def form():
+    return render_template("form.html")
+
+@app.route("/character", methods=["POST"])
+def character():
+    data = request.form
+
+    return render_template("character.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
